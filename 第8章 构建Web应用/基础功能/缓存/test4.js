4a02e95dc30a2dbfd289db55cf14ea43d3d40bf9
Pettarn <1225076678@qq.com>
2020-05-09 18:28:37 +0800

    bug: req to res

--- 第8章 构建Web应用/基础功能/缓存/test4.js
+++ 第8章 构建Web应用/基础功能/缓存/test4.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 let handle = function (req, res) {
     fs.readFile(filename, function (err, file) {
-        req.setHeader('Cache-Control', "max-age=" + 1000 * 60 * 60)
+        res.setHeader('Cache-Control', "max-age=" + 1000 * 60 * 60)
         res.writeHead(200, 'OK')
         res.end(file)
     })
