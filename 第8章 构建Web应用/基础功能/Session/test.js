4b2b0e4afee81e8ce52993d0548691f840c7cc22
Pettarn <1225076678@qq.com>
2020-05-16 15:55:42 +0800

    add: 基于cookie来实现用户和数据的映射

---
+++ 第8章 构建Web应用/基础功能/Session/test.js
@@ -0,0 +1,48 @@
+const sessions = {}
+const KEY = 'session_id'
+const EXPIRES = 20 * 60 * 1000
+
+const generate = function () {
+    const session = {}
+    session.id = new Date().getTime + Math.random()
+    session.cookie = {
+        expires: new Date().getTime() + EXPIRES
+    }
+    sessions[session.id] = session
+    return session
+}
+
+function core (req, res) {
+    let id = req.cookies[KEY]
+    if (!id) {
+        req.session = generate()
+    } else {
+        const session = sessions[id]
+        if (session) {
+            if (session.cookie.expires > new Date().getTime()) {
+                session.cookie.expires = new Date().getTime() + EXPIRES
+                req.session = session
+            } else {
+                delete sessions[id]
+                req.session = generate()
+            }
+        } else {
+            req.session = generate()
+        }
+    }
+    handle(req, res)
+}
+
+function handle (req, res) {
+    res.writeHead = function () {
+        const cookies = res.getHeaders['Set-Cookie']
+        const session = 
+    }
+    res.writeHead(200, 'OK')
+    if (!req.session.isVisit) {
+        req.session.isVisit = true
+        res.end('欢迎第一次访问！')
+    } else {
+        res.end('我们又见面了！')
+    }
+}
